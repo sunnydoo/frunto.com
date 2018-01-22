4e8077f974182d1dc6d90e03a60c8f7cd519e750
Robin Wang <jianpwan@gmail.com>
2018-01-22 23:02:02 +0800

    add table to swiper

--- page/component/pages/swinetools/swinetools.js
+++ page/component/pages/swinetools/swinetools.js
@@ -1,6 +1,17 @@
 Page({
   data: {
-    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+
+    headerItems: ["轻度", "中度", "重度"],
+    rowsOfKnows: [
+      { "name": "粪便虫卵指数", "mild": " <100", "moderate": "100 - 500", "severe": ">500"},
+      { "name": "粪便虫卵指数", "mild": " <100", "moderate": "100 - 500", "severe": ">500"},
+      { "name": "粪便虫卵指数", "mild": " <100", "moderate": "100 - 500", "severe": ">500" },
+      { "name": "粪便虫卵指数", "mild": " <100", "moderate": "100 - 500", "severe": ">500" },
+      { "name": "粪便虫卵指数", "mild": " <100", "moderate": "100 - 500", "severe": ">500" },
+      { "name": "粪便虫卵指数", "mild": " <100", "moderate": "100 - 500", "severe": ">500" },
+      { "name": "粪便虫卵指数", "mild": " <100", "moderate": "100 - 500", "severe": ">500" },
+    ],
+
     indicatorDots: true,
     vertical: false,
     autoplay: false,
